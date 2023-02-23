17534a0e0c13091daffd348e23b8fd4af975e82c
hans_csida <johann.csida@gmail.com>
2023-02-23 21:05:38 +0100

    feat: style cell

--- banky/banky/AccountSummary/AccountSummaryViewControler.swift
+++ banky/banky/AccountSummary/AccountSummaryViewControler.swift
@@ -42,6 +42,12 @@ extension AccountSummaryViewController {
         tableView.delegate = self
         tableView.dataSource = self
         
+        
+           tableView.register(AccountSummaryCell.self, forCellReuseIdentifier: AccountSummaryCell.reuseID)
+           tableView.rowHeight = AccountSummaryCell.rowHeight
+           tableView.tableFooterView = UIView()
+        
+        
         tableView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(tableView)
         
@@ -70,9 +76,13 @@ extension AccountSummaryViewController {
 
 
 extension AccountSummaryViewController: UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = UITableViewCell()
+//        cell.textLabel?.text = games[indexPath.row]
+//        return cell
+//    }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = games[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: AccountSummaryCell.reuseID, for: indexPath) as! AccountSummaryCell
         return cell
     }
     
